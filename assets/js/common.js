ff0b484386fcd9aaa93f4176ebd9cec846c1479b
mabo0526 <153962042+mabo0526@users.noreply.github.com>
2025-02-11 07:11:19 +0900

    [add]検索フォームと検索ページを追加しました。

--- assets/js/common.js
+++ assets/js/common.js
@@ -13,4 +13,15 @@ $(function () {
         $(".hamburger-menu").toggleClass("active");
         $(".header__navigation").toggleClass("open");
     });
+    $(".header__navigation--search_open_button").on('click', function () {
+        $(".search-window").addClass("search-window__visible");
+    });
+    $(".search-window__close").on('click', function () {
+        $(".search-window").removeClass("search-window__visible");
+    });
+    $(document).on('click', function (e) {
+        if (!$(e.target).closest("search-window__form").length && $(e.target).closest("search-window").length) {
+            $(".search-window").removeClass("search-window__visible");
+        }
+    });
 });
